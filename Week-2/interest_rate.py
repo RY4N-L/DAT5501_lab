6d67a0b80f084d8b11803c12ecf541003598fba2
RY4N-L <ryan.ly.lai@gmail.com>
2025-11-06 23:57:53 +0000

    Clean code and add comment for function

--- Week-2/interest_rate.py
+++ Week-2/interest_rate.py
@@ -1,14 +1,20 @@
 # Create a function that calculates the compound interest on a savings account.
 def CompoundInterestCalculator(savings, annualInterestRate, years):
+    """
+    Calculate the compound interest on a savings account.
+
+    Returns:
+    string: A summary of the savings after the specified number of years and the time to double the savings.
+    """
 
     multiply = annualInterestRate/100
     for i in range (years):
         savings *= 1 + multiply
-        text1 = "Your savings after " + str(i+1) + " years is " + str(savings)
-        print(text1)
+        #text1 = "Your savings after " + str(i+1) + " years is " + str(savings)
+        #print(text1)
     number_of_years_to_double = 72/annualInterestRate
 
-    text = "Your savings after " + str(years) + " years is " + str(savings) +". Your savings will double in " + str(number_of_years_to_double) + " years"
+    text = f"Your savings after {years} years is {savings} Your savings will double in {number_of_years_to_double} years"
     return text
 
 print(CompoundInterestCalculator(10000, 3.5, 10))
