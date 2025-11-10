c37ea4b6b1e2776b2ea74c594959638ef8514a97
RY4N-L <ryan.ly.lai@gmail.com>
2025-11-10 12:41:42 +0000

    Plot graph of time taken to sort price changes

--- Week-8/daily_price_change_sorting.py
+++ Week-8/daily_price_change_sorting.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import numpy as np
 
 # Load the CSV 
 asset_price_data_df = pd.read_csv('DAT5501_lab/Week-8/asset_price_data.csv')
@@ -11,10 +12,40 @@ asset_price_data_df = asset_price_data_df.sort_values(by='Date', ascending=True)
 # Setting to show all rows when printing the dataframe
 pd.set_option('display.max_rows', None)
 
-print (asset_price_data_df)
+#print (asset_price_data_df)
 
 # Calculate daily price change
 asset_price_data_df['Close/Last'] = asset_price_data_df['Close/Last'].replace('[\$,]', '', regex=True).astype(float) # Remove dollar signs and convert to float
 
 asset_price_data_df['Daily Price Change'] = asset_price_data_df['Close/Last'].diff()
-print (asset_price_data_df)
+#print (asset_price_data_df)
+
+
+# Move the price changes column to an array for plotting
+price_changes = asset_price_data_df['Daily Price Change'].dropna().to_numpy()
+times = []
+
+for i in range(7, len(price_changes)):
+    
+    array_to_sort = price_changes[:i]
+    
+    start_time = time.perf_counter()
+    # Sort the array
+    sorted_array = sorted(array_to_sort)
+    end_time = time.perf_counter()
+    
+    time_taken = end_time - start_time
+    times.append(time_taken)
+
+# Create x-axis values
+x_values = np.arange(7, len(times)+7)
+
+# Plot the graph
+plt.figure(figsize=(8, 4))
+plt.scatter(x_values, times, marker='o', linestyle='-', color='blue')
+plt.title('Daily Price Changes')
+plt.xlabel('Number of Price Changes Sorted')
+plt.ylabel('Price Change')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
